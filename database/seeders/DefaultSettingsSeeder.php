53c19e5f85913125e0d36dd21b3606cce5e6b44e
Simon Maraval <gti8sm@gmail.com>
2026-09-07 19:13:49 +0200

    Fix: add tenant_id=null to global settings seeder

--- database/seeders/DefaultSettingsSeeder.php
+++ database/seeders/DefaultSettingsSeeder.php
@@ -11,31 +11,31 @@ class DefaultSettingsSeeder extends Seeder
     {
         $settings = [
             // Paramètres généraux
-            ['key' => 'app_name', 'value' => 'Communeo', 'type' => 'string', 'group' => 'general', 'description' => 'Nom de l\'application'],
+            ['key' => 'app_name', 'value' => 'Communeo', 'type' => 'string', 'group' => 'general', 'description' => 'Nom de l\'application', 'tenant_id' => null],
             
             // Paramètres garderie
-            ['key' => 'garderie_morning_start', 'value' => '07:00', 'type' => 'string', 'group' => 'garderie', 'description' => 'Heure de début de la garderie du matin'],
-            ['key' => 'garderie_morning_end', 'value' => '08:30', 'type' => 'string', 'group' => 'garderie', 'description' => 'Heure de fin de la garderie du matin'],
-            ['key' => 'garderie_evening_start', 'value' => '16:30', 'type' => 'string', 'group' => 'garderie', 'description' => 'Heure de début de la garderie du soir'],
-            ['key' => 'garderie_evening_end', 'value' => '18:30', 'type' => 'string', 'group' => 'garderie', 'description' => 'Heure de fin de la garderie du soir'],
+            ['key' => 'garderie_morning_start', 'value' => '07:00', 'type' => 'string', 'group' => 'garderie', 'description' => 'Heure de début de la garderie du matin', 'tenant_id' => null],
+            ['key' => 'garderie_morning_end', 'value' => '08:30', 'type' => 'string', 'group' => 'garderie', 'description' => 'Heure de fin de la garderie du matin', 'tenant_id' => null],
+            ['key' => 'garderie_evening_start', 'value' => '16:30', 'type' => 'string', 'group' => 'garderie', 'description' => 'Heure de début de la garderie du soir', 'tenant_id' => null],
+            ['key' => 'garderie_evening_end', 'value' => '18:30', 'type' => 'string', 'group' => 'garderie', 'description' => 'Heure de fin de la garderie du soir', 'tenant_id' => null],
             
-            ['key' => 'smtp_host', 'value' => '', 'type' => 'string', 'group' => 'smtp', 'description' => 'Serveur SMTP'],
-            ['key' => 'smtp_port', 'value' => '587', 'type' => 'integer', 'group' => 'smtp', 'description' => 'Port SMTP'],
-            ['key' => 'smtp_username', 'value' => '', 'type' => 'string', 'group' => 'smtp', 'description' => 'Nom d\'utilisateur SMTP'],
-            ['key' => 'smtp_password', 'value' => '', 'type' => 'string', 'group' => 'smtp', 'description' => 'Mot de passe SMTP'],
-            ['key' => 'smtp_encryption', 'value' => 'tls', 'type' => 'string', 'group' => 'smtp', 'description' => 'Type de chiffrement'],
-            ['key' => 'smtp_from_address', 'value' => '', 'type' => 'string', 'group' => 'smtp', 'description' => 'Adresse email d\'expédition'],
-            ['key' => 'smtp_from_name', 'value' => 'Communeo', 'type' => 'string', 'group' => 'smtp', 'description' => 'Nom d\'expédition'],
+            ['key' => 'smtp_host', 'value' => '', 'type' => 'string', 'group' => 'smtp', 'description' => 'Serveur SMTP', 'tenant_id' => null],
+            ['key' => 'smtp_port', 'value' => '587', 'type' => 'integer', 'group' => 'smtp', 'description' => 'Port SMTP', 'tenant_id' => null],
+            ['key' => 'smtp_username', 'value' => '', 'type' => 'string', 'group' => 'smtp', 'description' => 'Nom d\'utilisateur SMTP', 'tenant_id' => null],
+            ['key' => 'smtp_password', 'value' => '', 'type' => 'string', 'group' => 'smtp', 'description' => 'Mot de passe SMTP', 'tenant_id' => null],
+            ['key' => 'smtp_encryption', 'value' => 'tls', 'type' => 'string', 'group' => 'smtp', 'description' => 'Type de chiffrement', 'tenant_id' => null],
+            ['key' => 'smtp_from_address', 'value' => '', 'type' => 'string', 'group' => 'smtp', 'description' => 'Adresse email d\'expédition', 'tenant_id' => null],
+            ['key' => 'smtp_from_name', 'value' => 'Communeo', 'type' => 'string', 'group' => 'smtp', 'description' => 'Nom d\'expédition', 'tenant_id' => null],
             
-            ['key' => 'notify_arrival', 'value' => '0', 'type' => 'boolean', 'group' => 'notifications', 'description' => 'Notifier les parents lors d\'une arrivée'],
-            ['key' => 'notify_departure', 'value' => '0', 'type' => 'boolean', 'group' => 'notifications', 'description' => 'Notifier les parents lors d\'un départ'],
-            ['key' => 'notify_absence', 'value' => '0', 'type' => 'boolean', 'group' => 'notifications', 'description' => 'Notifier en cas d\'absence'],
-            ['key' => 'notify_event', 'value' => '1', 'type' => 'boolean', 'group' => 'notifications', 'description' => 'Notifier en cas d\'événement'],
+            ['key' => 'notify_arrival', 'value' => '0', 'type' => 'boolean', 'group' => 'notifications', 'description' => 'Notifier les parents lors d\'une arrivée', 'tenant_id' => null],
+            ['key' => 'notify_departure', 'value' => '0', 'type' => 'boolean', 'group' => 'notifications', 'description' => 'Notifier les parents lors d\'un départ', 'tenant_id' => null],
+            ['key' => 'notify_absence', 'value' => '0', 'type' => 'boolean', 'group' => 'notifications', 'description' => 'Notifier en cas d\'absence', 'tenant_id' => null],
+            ['key' => 'notify_event', 'value' => '1', 'type' => 'boolean', 'group' => 'notifications', 'description' => 'Notifier en cas d\'événement', 'tenant_id' => null],
         ];
 
         foreach ($settings as $setting) {
             Setting::updateOrCreate(
-                ['key' => $setting['key']],
+                ['key' => $setting['key'], 'tenant_id' => null],
                 $setting
             );
         }
